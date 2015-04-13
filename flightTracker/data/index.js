f60a02be75bd3e779e6a6792d6fb06ce20939c6f
justin dobosh <justin.dobosh@spartans.ut.edu>
2015-04-13 17:01:05 -0400

    In Class Example
    Flight Tracker App | April 13/2015

---
+++ flightTracker/data/index.js
@@ -0,0 +1,93 @@
+module.exports = {
+	"13" : {
+		"number": 13,
+		"origin": "MKE",
+		"destination": "LAX",
+		"departs": "10:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"14" : {
+		"number": 14,
+		"origin": "PED",
+		"destination": "SAD",
+		"departs": "11:00 AM",
+		"arrives": "1:00 PM" 
+	},
+	"15" : {
+		"number": 15,
+		"origin": "EXA",
+		"destination": "SFO",
+		"departs": "1:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"16" : {
+		"number": 16,
+		"origin": "MKE",
+		"destination": "LAG",
+		"departs": "10:00 AM",
+		"arrives": "8:00 PM" 
+	},
+	"17" : {
+		"number": 17,
+		"origin": "HOG",
+		"destination": "LAX",
+		"departs": "10:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"18" : {
+		"number": 18,
+		"origin": "MKE",
+		"destination": "GRM",
+		"departs": "7:00 AM",
+		"arrives": "11:00 PM" 
+	},
+	"19" : {
+		"number": 19,
+		"origin": "MKE",
+		"destination": "SFO",
+		"departs": "10:00 AM",
+		"arrives": "5:00 PM" 
+	},
+	"20" : {
+		"number": 20,
+		"origin": "LAG",
+		"destination": "OAR",
+		"departs": "6:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"21" : {
+		"number": 21,
+		"origin": "OAR",
+		"destination": "TAP",
+		"departs": "10:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"22" : {
+		"number": 22,
+		"origin": "TAM",
+		"destination": "LAX",
+		"departs": "9:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"23" : {
+		"number": 23,
+		"origin": "ATR",
+		"destination": "XAR",
+		"departs": "2:00 AM",
+		"arrives": "12:00 PM" 
+	},
+	"24" : {
+		"number": 24,
+		"origin": "OAY",
+		"destination": "ZQP",
+		"departs": "10:00 AM",
+		"arrives": "11:00 PM" 
+	},
+	"25" : {
+		"number": 25,
+		"origin": "OAR",
+		"destination": "LAX",
+		"departs": "4:00 AM",
+		"arrives": "12:00 PM" 
+	}
+};
---
+++ flightTracker/flight/index.js
@@ -0,0 +1,35 @@
+//we use function scopes to get around module cashing 
+module.exports = function(info){
+
+	var values = {
+		number: null,
+		origin: null,
+		destination: null,
+		departs: null,
+		arrives: null,
+		actualDepart: null,
+		actualArrive: null
+	};//end of values object
+
+	for(var prop in values){
+		if(values[prop] !== 'undefined'){
+			values[prop] = info[prop];
+		}//end of if statement
+	}//end for loop
+
+	var functions = {
+		triggerDepart: function(){
+			values.actualDepart = Date.now();
+		},
+		triggerArrive: function(){
+			values.actualArrive = Date.now();
+		},
+		getInformation: function(){
+			return values
+		}
+	};//end of functions object
+
+	return functions;
+	//returns the functions object
+
+};//end of module
